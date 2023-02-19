4ed1bb1c2ef8691f8f6962a2f3b075476f900cc3
aion0721 <rudolf.aion@gmail.com>
2023-02-19 19:07:39 +0900

    check logs dir / output logs

--- server.sh
+++ server.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# define
 PORT=4000
+LOG_OUT="./logs/stdout.log"
+LOG_ERR="./logs/stderr.log"
 
+# Function
 function response() {
 	echo "HTTP/1.0 200 OK"
 	echo "Connect-Type: text/plain"
@@ -11,8 +15,16 @@ function response() {
 
 }
 
+# Check 
+if [ ! -d ./logs ]; then
+	mkdir ./logs
+fi
+
 # main
 trap exit INT
+
+exec 1> >(tee -a $LOG_OUT)
+exec 2> >(tee -a $LOG_ERR)
 
 while true; do
 	response | nc -l "$PORT" -w 1
